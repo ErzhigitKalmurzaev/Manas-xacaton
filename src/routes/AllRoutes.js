e3d6dae3d300c385ced1e963096b59283a9f01e5
Erzhigit <96490425+ErzhigitKalmurzaev@users.noreply.github.com>
2024-04-19 22:17:25 +0600

    a

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -3,11 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import UserLayouts from '../layout/UserLayouts'
 import DepartmentRoutes from './DepartmentRoutes'
 import StudentApplicationForm from '../components/StudentApplicationForm'
+import UserRoutes from './UserRoutes'
 
 const AllRoutes = () => {
+
+
   return (
     <Routes>
-      <Route path='/' element={<UserLayouts />}/>
+      <Route path='/' element={<UserRoutes />}/>
       <Route path='/test' element={<StudentApplicationForm />}></Route>  
       <Route path='/department/*' element={<DepartmentRoutes />}>  
        
